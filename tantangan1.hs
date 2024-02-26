9ed0bb8b14d6842d9c173fd8a1156c48a0f22c6a
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2024-02-26 15:20:05 +0700

    Week 5 Haskell

--- tantangan1.hs
+++ tantangan1.hs
@@ -1,105 +1,241 @@
--- Nama = Glen Rejeki Sitorus
--- NIM = 11S23024
--- Prodi = Informatika
---
---
---
---                                 
+{--
+    nama : Glen Rejeki Sitorus
+    NIM : 11S23024
+    prodi : Informatika
+--}
 
-data Pecahan = Pecahan {a::Int, b::Int} deriving Show
 
+
+data Date=Date {dd::Int, mm::Int, yy::Int} deriving Show
 -- Selektor
-pemb::Pecahan ->Int
-pemb pecahan = a pecahan
+day::Date -> Int    
+day date = dd date  
 
-penny::Pecahan -> Int
-penny pecahan = b pecahan
+month::Date -> Int
+month date = mm date
+
+year:: Date -> Int
+year date = yy date
 
 -- Konstruktor
-makeP:: Int->Int->Pecahan
-makeP a b = Pecahan{a=a, b=b}
+makeDate :: Int -> Int -> Int -> Date
+makeDate dd mm yy = Date{dd=dd, mm=mm, yy=yy}
 
+-- Fungsi untuk mendapatkan jumlah hari dalam sebuah bulan 
+daysInMonth :: Int -> Int -> Int
+daysInMonth month year
+    | month `elem` [1, 3, 5, 7, 8, 10, 12] = 31
+    | month `elem` [4, 6, 9, 11]           = 30
+    | month == 2                           = if isKabisat year then 29 else 28
+    | otherwise                            = 0
 -- Operator
-addP::Pecahan -> Pecahan->Pecahan
-addP p1 p2 = makeP (pemb p1 *penny p2  + pemb p2 *penny p1 ) (penny p1 *penny p2)
+nextDay :: Date -> Date
+nextDay (Date dd mm yy)
+    | dd < daysInMonth mm yy = Date (dd + 1) mm yy -- Tanggal berikutnya di dalam bulan yang sama
+    | mm < 12                = Date 1 (mm + 1) yy  -- Tanggal berikutnya di bulan berikutnya dalam tahun yang sama
+    | otherwise              = Date 1 1 (yy + 1)   -- Tanggal berikutnya di bulan Januari tahun berikutnya
 
-subP::Pecahan -> Pecahan ->Pecahan
-subP p1 p2 = makeP (pemb p1 *penny p2  - pemb p2 *penny p1 ) (penny p1 *penny p2)
-mulP::Pecahan -> Pecahan ->Pecahan
-mulP p1 p2 = makeP (pemb p1 * pemb p2) (penny p1 * penny p2)
+yesterday :: Date -> Date
+yesterday (Date d m y)
+    | d > 1     = makeDate (d - 1) m y
+    | m > 1     = makeDate (daysInMonth (m - 1) y) (m - 1) y
+    | otherwise = makeDate 31 12 (y - 1)
 
-divP:: Pecahan -> Pecahan -> Pecahan 
-divP p1 p2 = makeP (pemb p1 * penny p2) (penny p1 * pemb p2 )
+addDay :: Date -> Int -> Date
+addDay date n = iterate nextDay date !! n
+  where
+    nextDay :: Date -> Date
+    nextDay (Date d m y)
+      | d < daysInMonth m y = Date (d + 1) m y
+      | m < 12              = Date 1 (m + 1) y
+      | otherwise           = Date 1 1 (y + 1)
 
--- Predikat
-isEqP::Pecahan -> Pecahan -> Bool
-isEqP p1 p2 = pemb p1 * penny p2 == penny p1 * pemb p2
+subDay :: Date -> Int -> Date
+subDay date n = if n == 0 then date 
+                else if year date <0 then Date {dd = 1, mm=1 ,yy = 1} 
+                else subDay (yesterday date ) (n-1)
 
-isLtP::Pecahan -> Pecahan -> Bool
-isLtP p1 p2  = pemb p1 * penny p2 < penny p1 * pemb p2
+nextMonth :: Date -> Date
+nextMonth (Date d m y)
+    | m < 12    = makeDate d (m + 1) y
+    | otherwise = makeDate d 1 (y + 1)
+
+prevMonth :: Date -> Date
+prevMonth (Date d m y)
+    | m > 1     = makeDate d (m - 1) y
+    | otherwise = makeDate d 12 (y - 1)
+
+addMonth :: Date -> Int -> Date
+addMonth (Date d m y) n = iterate nextMonth (Date d m y) !! n
+    where
+        nextMonth :: Date -> Date
+        nextMonth (Date d m y)
+            | m < 12    = makeDate d (m + 1) y
+            | otherwise = makeDate d 1 (y + 1)
+
+subMonth :: Date -> Int -> Date
+subMonth date n = if n == 0 then date 
+                    else if year date <0 then Date {dd= 1, mm=1, yy=1}
+                    else subMonth (prevMonth date) (n-1)
+
+addYear :: Date -> Int -> Date
+addYear (Date d m y) n = Date d m (y + n)
+
+subYear :: Date -> Int -> Date 
+subYear date n = if n == 0 then date
+                        else if year date < n then Date {dd = 1, mm=1, yy = 1 }
+                        else Date {dd = day date, mm = month date, yy = year date - n }
+
+-- -- Predikat
+isEqD :: Date -> Date -> Bool
+isEqD (Date d1 m1 y1) (Date d2 m2 y2) = d1 == d2 && m1 == m2 && y1 == y2 
+
+isNotEqD :: Date -> Date -> Bool
+isNotEqD (Date d1 m1 y1) (Date d2 m2 y2) = d1 /= d2 ||m1 /= m2 || y1 /= y2
+
+isBefore :: Date -> Date -> Bool
+isBefore date1 date2 = isEqD (yesterday date2) (date1)
+
+isNext :: Date -> Date -> Bool 
+isNext date1 date2 = isEqD (nextDay date2) (date1)
 
 
-main::IO()
+-- -- Fungsi untuk mengecek apakah tanggal d1 sebelum d2 sebanyak n hari
+isBeforeN :: Date -> Date -> Int -> Bool
+isBeforeN date1 date2 n= isEqD (subDay  date2 n) (date1)
+
+isNextN :: Date -> Date -> Int -> Bool
+isNextN date1 date2 n =isEqD (addDay date2 n) (date1)
+
+
+-- Fungsi Lain
+isKabisat :: Int -> Bool
+isKabisat yy
+    | yy `mod` 400 == 0 = True
+    | yy `mod` 4 == 0 && yy `mod` 100 /= 0 = True
+    | otherwise = False
+
+
+
+main::IO ()
 main = do 
-    pem1<-readLn::IO Int
-    pen1<- readLn :: IO Int
-    pem2 <- readLn :: IO Int
-    pen2 <- readLn :: IO Int
+        dd1 <- readLn :: IO Int
+        mm1 <- readLn :: IO Int
+        yy1 <- readLn :: IO Int 
+        dd2 <- readLn :: IO Int
+        mm2 <- readLn :: IO Int 
+        yy2 <- readLn :: IO Int
+        n <- readLn :: IO Int
 
-    let p1 = makeP pem1 pen1
-        p2 = makeP pem2 pen2
-    
-    putStrLn $ "p1 = " ++ show p1
-    putStrLn $ "p2 = " ++ show p2 
+        let d1 = makeDate dd1 mm1 yy1 
+            d2 = makeDate dd2 mm2 yy2
+        
+        putStrLn $ "d1 = " ++show d1
+        putStrLn $ "d2 = " ++show d2
+        putStrLn $ "n = " ++ show n
+        
+        putStrLn $ "nextDay(d1) = " ++ show (nextDay d1)
+        putStrLn $ "nextDay(d2) = " ++ show (nextDay d2)
 
-    putStrLn $ "addP(p1, p2) = " ++ show (addP p1 p2)
 
-    putStrLn $ "subP(p1, p2) = "++show (subP p1 p2)
+        putStrLn $ "yesterday(d1) = "++show (yesterday d1)
+        putStrLn $ "yesterday(d2) = "++show (yesterday d2)
+        
+        putStrLn $ "addDay(d1, n) = " ++ show (addDay d1 n)
+        putStrLn $ "addDay(d2, n) = " ++ show (addDay d2 n)
 
-    putStrLn $ "mulP(p1, p2) = "++show (mulP p1 p2)
-  
-    putStrLn $ "divP(p1, p2) = "++show (divP p1 p2)
+        putStrLn $ "subDay(d1, n) = "++show(subDay d1 n)
+        putStrLn $ "subDay(d2, n) = "++show (subDay d2 n)
 
-    putStrLn $ "isEqP(p1, p2) = "++show (isEqP p1 p2)
+        putStrLn $ "nextMonth(d1) = "++show (nextMonth d1)
+        putStrLn $ "nextMonth(d2) = "++show (nextMonth d2)
 
-    putStrLn $ "isLtP(p1, p2) = "++show (isLtP p1 p2)
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
---
+        putStrLn $ "prevMonth(d1) = "++show (prevMonth d1)
+        putStrLn $ "prevMonth(d2) = "++show (prevMonth d2)
+
+        putStrLn $ "addMonth(d1, n) = " ++ show (addMonth d1 n)
+        putStrLn $ "addMonth(d2, n) = " ++ show (addMonth d2 n)
+
+
+        putStrLn $ "subMonth(d1, n) = " ++ show (subMonth d1 n)
+        putStrLn $ "subMonth(d2, n) = " ++ show (subMonth d2 n)
+
+        putStrLn $ "addYear(d1, n) = "++show (addYear d1 n)
+        putStrLn $ "addYear(d2, n) = "++show (addYear d2 n)
+
+        putStrLn $ "subYear(d1, n) = "++show (subYear d1 n)
+        putStrLn $ "subYear(d2, n) = "++show (subYear d2 n)
+
+        putStrLn $ "isEqD(d1, d2) = "++show (isEqD d1 d2)
+
+        putStrLn $ "isNotEqD(d1, d2) = "++show(isNotEqD d1 d2)
+
+        putStrLn $ "isBefore(d1, d2) = "++show(isBefore d1 d2)
+
+        putStrLn $ "isNext(d1, d2) = " ++ show (isNext d1 d2)
+
+        putStrLn $ "isBeforeN(d1, d2, n) = "++show(isBeforeN d1 d2 n)
+
+        putStrLn $ "isNextN(d1, d2, n) = "++show(isNextN d1 d2 n)
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
+        --
